21ae153ef05e353630ad075f08408d54a6f80d7e
David Son <sonkdav@gmail.com>
2023-03-28 22:55:50 -0700

    Updated ping.js
    Refactored the code to v14. Command works on morale-dev.

--- src/commands/util/ping.js
+++ src/commands/util/ping.js
@@ -1,10 +1,33 @@
-const { SlashCommandBuilder } = require("discord.js");
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
 
 module.exports = {
+
+	//Creating the command and description.
 	data: new SlashCommandBuilder()
 		.setName("ping")
 		.setDescription("Replies with Pong!"),
+	
+	//Functionality of the command.
 	async execute(interaction) {
-		await interaction.reply("Pong!");
+		await interaction.reply({ 
+			content: 'Calculating Ping...' 
+		});
+
+		const reply = await interaction.fetchReply();
+		const botLatency = reply.createdTimestamp - interaction.createdTimestamp;
+		const apiLatency = Math.round(interaction.client.ws.ping);
+		
+		const pingEmbed = new EmbedBuilder()
+			.setColor('#4fffa4')
+			.setTitle('🏓 Pong!')
+			.addFields(
+				{name: 'Bot Latency', value: `**${botLatency}ms**`},
+				{name: 'API Latency', value: `**${apiLatency}ms**`},
+			)
+
+		return await interaction.editReply({
+			content : 'Ping Calculated.', 
+			embeds: [pingEmbed] 
+		});
 	},
 };
