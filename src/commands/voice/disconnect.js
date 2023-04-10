0a90d3b2a1f9145f2045f06504a81da9d09f7c95
Ian Tjok <ianptjok@hotmail.com>
2023-04-10 13:23:15 -0700

    fixed tab length to 4 spaces

--- src/commands/voice/disconnect.js
+++ src/commands/voice/disconnect.js
@@ -9,31 +9,31 @@ const { SlashCommandBuilder } = require("discord.js");
 */
 
 module.exports = {
-  data: new SlashCommandBuilder()
-    .setName("disconnect")
-    .setDescription("Disconnect the bot from voice channel"),
-  async execute(interaction) {
-    const userVoiceChannel = interaction.member.voice.channel;
+    data: new SlashCommandBuilder()
+        .setName("disconnect")
+        .setDescription("Disconnect the bot from voice channel"),
+    async execute(interaction) {
+        const userVoiceChannel = interaction.member.voice.channel;
 
-    // Check if user is connected to a voice channel
-    if (!userVoiceChannel) {
-      return await interaction.reply(
-        "You must be in a voice channel to use the disconnect command."
-      );
-    }
+        // Check if user is connected to a voice channel
+        if (!userVoiceChannel) {
+            return await interaction.reply(
+                "You must be in a voice channel to use the disconnect command."
+            );
+        }
 
-    // Get the bot voice connection and destroy it
-    const connection = getVoiceConnection(userVoiceChannel.guild.id);
+        // Get the bot voice connection and destroy it
+        const connection = getVoiceConnection(userVoiceChannel.guild.id);
 
-    if (!connection) {
-      return await interaction.reply(
-        "The bot must be connected to a voice channel before using the disconnect command."
-      );
-    }
+        if (!connection) {
+            return await interaction.reply(
+                "The bot must be connected to a voice channel before using the disconnect command."
+            );
+        }
 
-    connection.destroy();
-    return await interaction.reply(
-      "MoraleBot has been disconnected from the voice channel."
-    );
-  },
+        connection.destroy();
+        return await interaction.reply(
+            "MoraleBot has been disconnected from the voice channel."
+        );
+    },
 };
--- src/commands/voice/play.js
+++ src/commands/voice/play.js
@@ -1,10 +1,10 @@
 const fs = require("fs");
 const { join } = require("node:path");
 const {
-  getVoiceConnection,
-  joinVoiceChannel,
-  createAudioPlayer,
-  createAudioResource,
+    getVoiceConnection,
+    joinVoiceChannel,
+    createAudioPlayer,
+    createAudioResource,
 } = require("@discordjs/voice");
 const { SlashCommandBuilder } = require("discord.js");
 
@@ -15,118 +15,120 @@ const { SlashCommandBuilder } = require("discord.js");
 */
 
 module.exports = {
-  data: new SlashCommandBuilder()
-    .setName("play")
-    .setDescription("Choose an Audio Category.")
-    .addStringOption((option) =>
-      option
-        .setName("category")
-        .setDescription("choose an audio option")
-        .setRequired(true)
-        .addChoices(
-          { name: "David", value: "david" },
-          { name: "Joey", value: "joey" },
-          { name: "Goddammit", value: "goddammit" },
-          { name: "Bonk", value: "bonk" },
-          { name: "Bingbong", value: "bingbong" },
-          { name: "Animal", value: "animal" },
-          { name: "Pepperoncini", value: "pepperoncini" }
-        )
-    ),
+    data: new SlashCommandBuilder()
+        .setName("play")
+        .setDescription("Choose an Audio Category.")
+        .addStringOption((option) =>
+            option
+                .setName("category")
+                .setDescription("choose an audio option")
+                .setRequired(true)
+                .addChoices(
+                    { name: "David", value: "david" },
+                    { name: "Joey", value: "joey" },
+                    { name: "Goddammit", value: "goddammit" },
+                    { name: "Bonk", value: "bonk" },
+                    { name: "Bingbong", value: "bingbong" },
+                    { name: "Animal", value: "animal" },
+                    { name: "Pepperoncini", value: "pepperoncini" }
+                )
+        ),
 
-  async execute(interaction) {
-    // Grab the voice channel and request from the User
-    const channel = interaction.member.voice.channel;
+    async execute(interaction) {
+        // Grab the voice channel and request from the User
+        const channel = interaction.member.voice.channel;
 
-    if (!channel) {
-      return await interaction.reply(
-        "You must be in a voice channel to use the play command."
-      );
-    }
+        if (!channel) {
+            return await interaction.reply(
+                "You must be in a voice channel to use the play command."
+            );
+        }
 
-    let playRequest = interaction.options.getString("category");
+        let playRequest = interaction.options.getString("category");
 
-    // Determine the file path for the given request
-    let filePath;
-    switch (playRequest) {
-      case "david":
-        const files = fs.readdirSync("./assets/sounds/david/");
-        const randomDavidSound =
-          files[Math.floor(Math.random() * files.length)];
+        // Determine the file path for the given request
+        let filePath;
+        switch (playRequest) {
+            case "david":
+                const files = fs.readdirSync("./assets/sounds/david/");
+                const randomDavidSound =
+                    files[Math.floor(Math.random() * files.length)];
 
-        filePath = `../../assets/sounds/david/${randomDavidSound}`;
+                filePath = `../../assets/sounds/david/${randomDavidSound}`;
 
-        break;
+                break;
 
-      case "animal":
-        const animalFiles = fs.readdirSync("./assets/sounds/animal/");
-        const randomAnimalSound =
-          animalFiles[Math.floor(Math.random() * animalFiles.length)];
+            case "animal":
+                const animalFiles = fs.readdirSync("./assets/sounds/animal/");
+                const randomAnimalSound =
+                    animalFiles[Math.floor(Math.random() * animalFiles.length)];
 
-        filePath = `../../assets/sounds/animal/${randomAnimalSound}`;
+                filePath = `../../assets/sounds/animal/${randomAnimalSound}`;
 
-        break;
+                break;
 
-      case "joey":
-        filePath = `../../assets/sounds/david/joey.wav`;
+            case "joey":
+                filePath = `../../assets/sounds/david/joey.wav`;
 
-        break;
+                break;
 
-      case "goddammit":
-        filePath = `../../assets/sounds/david/godDammit.wav`;
+            case "goddammit":
+                filePath = `../../assets/sounds/david/godDammit.wav`;
 
-        break;
+                break;
 
-      case "pepperoncini":
-        filePath = `../../assets/sounds/general/pepperoncini.mp3`;
+            case "pepperoncini":
+                filePath = `../../assets/sounds/general/pepperoncini.mp3`;
 
-        break;
+                break;
 
-      case "bonk":
-        filePath = `../../assets/sounds/general/bonkSound.mp3`;
+            case "bonk":
+                filePath = `../../assets/sounds/general/bonkSound.mp3`;
 
-        break;
+                break;
 
-      case "bingbong":
-        filePath = `../../assets/sounds/general/bingBong.mp3`;
+            case "bingbong":
+                filePath = `../../assets/sounds/general/bingBong.mp3`;
 
-        break;
+                break;
 
-      default:
-        return await interaction.reply("Error: could not find audio category");
-    }
+            default:
+                return await interaction.reply(
+                    "Error: could not find audio category"
+                );
+        }
 
-    // Get the bot voice connection and destroy it
-    let connection = getVoiceConnection(channel.guild.id);
+        // Get the bot voice connection and destroy it
+        let connection = getVoiceConnection(channel.guild.id);
 
-    if (!connection) {
-      // DiscordJS Voice Connections and Audio Player Setup
-      connection = joinVoiceChannel({
-        channelId: channel.id,
-        guildId: channel.guild.id,
-        adapterCreator: channel.guild.voiceAdapterCreator,
-      });
+        if (!connection) {
+            // DiscordJS Voice Connections and Audio Player Setup
+            connection = joinVoiceChannel({
+                channelId: channel.id,
+                guildId: channel.guild.id,
+                adapterCreator: channel.guild.voiceAdapterCreator,
+            });
 
-      await interaction.channel.send("Connected to a voice channel.");
-    }
+            await interaction.channel.send("Connected to a voice channel.");
+        }
 
-    const player = createAudioPlayer();
+        const player = createAudioPlayer();
 
-    let resource = createAudioResource(join(__dirname, `${filePath}`), {
-      inlineVolume: true,
-    });
-    resource.volume.setVolume(0.5);
+        let resource = createAudioResource(join(__dirname, `${filePath}`), {
+            inlineVolume: true,
+        });
+        resource.volume.setVolume(0.5);
 
-    // Subscribe the connection to the audio player (will play audio on the voice connection)
-    const subscription = connection.subscribe(player);
+        // Subscribe the connection to the audio player (will play audio on the voice connection)
+        const subscription = connection.subscribe(player);
 
-    // subscription could be undefined if the connection is destroyed
-    if (subscription) {
-      // Unsubscribe after 5 seconds (stop playing audio on the voice connection)
-      setTimeout(() => subscription.unsubscribe(), 5_000);
-    }
-    player.play(resource);
+        // subscription could be undefined if the connection is destroyed
+        if (subscription) {
+            // Unsubscribe after 5 seconds (stop playing audio on the voice connection)
+            setTimeout(() => subscription.unsubscribe(), 5_000);
+        }
+        player.play(resource);
 
-    return await interaction.reply(`Now playing ${playRequest} audio.`);
-  },
+        return await interaction.reply(`Now playing ${playRequest} audio.`);
+    },
 };
