18fca817f5fe218197a2ed9b6b38a92f1fc31917
Ian Tjok <ianptjok@hotmail.com>
2023-04-20 22:07:41 -0700

    added rng to coinflip.js
    - no longer 100% heads
    - uses Math.random() to simulate rng

--- src/commands/fun/coinflip.js
+++ src/commands/fun/coinflip.js
@@ -1,136 +1,140 @@
 const {
-	SlashCommandBuilder,
-	ActionRowBuilder,
-	ButtonBuilder,
-	ButtonStyle,
-	ComponentType,
+    SlashCommandBuilder,
+    ActionRowBuilder,
+    ButtonBuilder,
+    ButtonStyle,
+    ComponentType,
 } = require("discord.js");
 
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName("coinflip")
-		.setDescription("Heads or tails? Only one way to find out.")
-		.addIntegerOption((option) =>
-			option
-				.setName("betting")
-				.setDescription(
-					"Are you willing to back up your guess with some Morale?"
-				)
-		),
+    data: new SlashCommandBuilder()
+        .setName("coinflip")
+        .setDescription("Heads or tails? Only one way to find out.")
+        .addIntegerOption((option) =>
+            option
+                .setName("betting")
+                .setDescription(
+                    "Are you willing to back up your guess with some Morale?"
+                )
+        ),
 
-	async execute(interaction) {
-		const bet = interaction.options.getInteger("betting");
-		let msg = "";
-		if (bet != null) {
-			msg = `You bet ${bet} Morale! `;
-		}
-		const button = new ActionRowBuilder()
-			.addComponents(
-				new ButtonBuilder()
-					.setCustomId("heads")
-					.setLabel("Heads!")
-					.setStyle(ButtonStyle.Primary)
-			)
-			.addComponents(
-				new ButtonBuilder()
-					.setCustomId("tails")
-					.setLabel("Tails!")
-					.setStyle(ButtonStyle.Primary)
-			)
-			.addComponents(
-				new ButtonBuilder()
-					.setCustomId("cancel")
-					.setLabel("❌ Cancel!")
-					.setStyle(ButtonStyle.Secondary)
-			);
+    async execute(interaction) {
+        const bet = interaction.options.getInteger("betting");
+        let msg = "";
+        if (bet != null) {
+            msg = `You bet ${bet} Morale! `;
+        }
+        const button = new ActionRowBuilder()
+            .addComponents(
+                new ButtonBuilder()
+                    .setCustomId("heads")
+                    .setLabel("Heads!")
+                    .setStyle(ButtonStyle.Primary)
+            )
+            .addComponents(
+                new ButtonBuilder()
+                    .setCustomId("tails")
+                    .setLabel("Tails!")
+                    .setStyle(ButtonStyle.Primary)
+            )
+            .addComponents(
+                new ButtonBuilder()
+                    .setCustomId("cancel")
+                    .setLabel("❌ Cancel!")
+                    .setStyle(ButtonStyle.Secondary)
+            );
 
-		await interaction.reply({
-			content: msg + "\n What will you choose? Heads or tails?",
-			components: [button],
-		});
+        await interaction.reply({
+            content: msg + "\n What will you choose? Heads or tails?",
+            components: [button],
+        });
 
-		// fetches the reply and creates a collector for user's response
-		const message = await interaction.fetchReply();
-		const collector = message.createMessageComponentCollector({
-			componentType: ComponentType.Button,
-			time: 10000,
-		});
+        // fetches the reply and creates a collector for user's response
+        const message = await interaction.fetchReply();
+        const collector = message.createMessageComponentCollector({
+            componentType: ComponentType.Button,
+            time: 10000,
+        });
 
-    // simulates flipping a coin through rng
-    // will implement rng in the future, for now results will always be heads
-    let results = "heads";
+        // simulates flipping a coin through rng
+        let rng = Math.floor(Math.random() * 2);
 
-		// Collection event: Handles game logic
-		collector.on("collect", async (collected) => {
-			// checks for a win
-			const choice = collected.customId;
-			const isWin = results == choice;
+        let results = "heads";
+        if (rng == 1) {
+            results = "tails";
+        }
 
-			// user chooses cancel button
-			if (choice == "cancel") {
-				return await interaction.editReply({
-					content: `
+        // Collection event: Handles game logic
+        collector.on("collect", async (collected) => {
+            // checks for a win
+            const choice = collected.customId;
+            const isWin = results == choice;
+
+            // user chooses cancel button
+            if (choice == "cancel") {
+                return await interaction.editReply({
+                    content: `
 						> __**Game Cancelled**__
 						> Any bets have been refunded.`,
-					components: [],
-				});
-			}
+                    components: [],
+                });
+            }
 
-			// Set colors and disable all buttons according touser response
-			button.components.forEach((component) => {
-				if (component.data.custom_id == choice) {
-					if (isWin) {
-						component.setStyle("Success");
-					} else {
-						component.setStyle("Danger");
-					}
-				}
-				component.setDisabled(true);
-			});
+            // Set colors and disable all buttons according touser response
+            button.components.forEach((component) => {
+                if (component.data.custom_id == choice) {
+                    if (isWin) {
+                        component.setStyle("Success");
+                    } else {
+                        component.setStyle("Danger");
+                    }
+                }
+                component.setDisabled(true);
+            });
 
-			// Win/lose response
-			if (collected.user.id === interaction.user.id) {
-				if (isWin) {
-					if (bet != null) {
-						collected.update({
-							content: `The coin landed on ${results}! \nCongratulations, you won ${bet} Morale!`,
-							components: [button],
-						});
-					} else {
-						collected.update({
-							content: `The coin landed on ${results}! \nExcellent guess!`,
-							components: [button],
-						});
-					}
-				} else {
-					if (bet != null) {
-						collected.update({
-							content: `The coin landed on ${results}! \nUnfortunately, you lost ${bet} Morale!\nBetter luck next time...`,
-							components: [button],
-						});
-					} else {
-						collected.update({
-							content: `The coin landed on ${results}! \nBetter luck next time!`,
-							components: [button],
-						});
-					}
-				}
-			}
-		});
+            // Win/lose response
+            if (collected.user.id === interaction.user.id) {
+                if (isWin) {
+                    if (bet != null) {
+                        collected.update({
+                            content: `The coin landed on ${results}! \nCongratulations, you won ${bet} Morale!`,
+                            components: [button],
+                        });
+                    } else {
+                        collected.update({
+                            content: `The coin landed on ${results}! \nExcellent guess!`,
+                            components: [button],
+                        });
+                    }
+                } else {
+                    if (bet != null) {
+                        collected.update({
+                            content: `The coin landed on ${results}! \nUnfortunately, you lost ${bet} Morale!\nBetter luck next time...`,
+                            components: [button],
+                        });
+                    } else {
+                        collected.update({
+                            content: `The coin landed on ${results}! \nBetter luck next time!`,
+                            components: [button],
+                        });
+                    }
+                }
+            }
+        });
 
-		// Collector End Event
-		collector.on("end", async (collected) => {
-			console.log(`Collected ${collected.size} interactions.`);
-			// user took too longer than 10 seconds to respond
-			if (collected.size == 0) {
-				return await interaction.editReply({
-					content: `
+        // Collector End Event
+        collector.on("end", async (collected) => {
+            console.log(`Collected ${collected.size} interactions.`);
+            // user took too longer than 10 seconds to respond
+            if (collected.size == 0) {
+                return await interaction.editReply({
+                    content: `
 						> __**Game Cancelled**__
 						> Any bets have been refunded.
 						> Please click on the Heads or Tails buttons. The time limit is 10 seconds.`,
-					components: [],
-				});
-			}
-		});
-	},
+                    components: [],
+                });
+            }
+        });
+    },
 };
